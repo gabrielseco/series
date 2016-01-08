a014aaf92b71674739c270b982815332e32f62bc
Gabriel García Seco <ggarciaseco@gmail.com>
2016-01-08 23:16:25 +0100

    Table component

--- src/scripts/components/Interfaces/Episodes.jsx
+++ src/scripts/components/Interfaces/Episodes.jsx
@@ -1,6 +1,88 @@
 import React from 'react';
 import {getAllEpisodes} from '../../actions'
 import { connect } from 'react-redux';
+import UITable from '../UI/Table'
+
+const columns = [
+      {
+          property: 'nombre',
+          header: 'Nombre'
+      },
+      {
+          property: 'numero',
+          header: 'Número'
+      },
+      {
+          property: 'ver',
+          header: 'Ver',
+          cell: (value, data, rowIndex, property) => {
+             var ver = () => {
+               var idEpisodio = data[rowIndex].id;
+               console.log('id', idEpisodio);
+               //console.log('ver palabras de un capítulo'+id);
+              // this.transitionTo('/dictionaryEpisode/:idPelicula/:idEpisodio', {idPelicula: id, idEpisodio: idEpisodio} );
+
+             };
+
+             return {
+                 value: <span>
+                     <a onClick={ver} className="edit-btn">ver</a>
+                 </span>
+             };
+           }
+      },
+      {
+        property: 'editar',
+        header: 'Editar',
+        cell: (value, data, rowIndex, property) => {
+           var editar = () => {
+             /*var id = data[rowIndex].ID;
+             var idSerie = this.getParams().id;
+             var idGenerator = this.getParams().idGenerator;
+
+
+             this.transitionTo('/modifyEpisode/:id/:idSerie/:idGenerator', {id: id, idSerie: idSerie, idGenerator: idGenerator });
+             */
+           };
+
+           return {
+               value: <span>
+                   <a onClick={editar} className="edit-btn">Editar</a>
+               </span>
+           };
+         }
+       },
+       {
+         property: 'eliminar',
+         header: 'Eliminar',
+         cell: (value, data, rowIndex, property) => {
+            var eliminar = () => {
+              var id = data[rowIndex].ID;
+              var nombre = data[rowIndex].Nombre;
+              var numero = data[rowIndex].Numero;
+              //console.log(nombre+" "+id+" "+numero);
+
+              /*var del = confirm('Quieres eliminar el episodio numero '+numero+ ' con nombre: '+nombre);
+
+              if(del){
+                this.props.flux.getActions('tv').deleteEpisode(id).then(function(res){
+                  //console.log('res delete', res);
+                  if(res[0].Resultado === 200){
+                    location.reload();
+                  }
+                });
+              }*/
+            };
+
+            return {
+                value: <span>
+                    <a onClick={eliminar} className="delete-btn">Eliminar</a>
+                </span>
+            };
+          }
+        }
+
+    ];
 
 class Episodes extends React.Component {
   componentDidMount(){
@@ -8,10 +90,36 @@ class Episodes extends React.Component {
     dispatch(getAllEpisodes(this.props.params.id))
   }
   render(){
+    const { episodes } = this.props;
+    const pagination = {
+        page: 0,
+        perPage: 10
+    }
+
+    const search = {
+           column: '',
+           query: ''
+    }
+
+    if(episodes.length > 0){
+
     return(
-      <div>hola vecinos {this.props.params.id}</div>
+      <div className="table-react">
+        <div className='img-info'>
+          <img src={episodes[0].serie.imagen}/>
+          BREADCRUMB
+        </div>
+        <div className="dictionaryButton">
+              <button onClick={this.addEpisodes}>ADD EPISODES</button>
+              <button onClick={this.generateEpisodes}>GENERATE</button>
+        </div>
+        <UITable data={episodes} columns={columns} pagination={pagination} search={search}/>
+      </div>
     );
+  } else {
+    return (<div></div>)
   }
+}
 
 }
 function mapStateToProps(state) {
---
+++ src/scripts/components/UI/Table.js
@@ -0,0 +1,108 @@
+import React from 'react';
+import { Table, Search, sortColumn } from 'reactabular';
+import Paginator from 'react-pagify';
+
+
+class UITable extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = { columns: this.props.columns,
+                   data: this.props.data,
+                   pagination: this.props.pagination,
+                   search:this.props.search,
+                   header: {
+                    onClick: (column) => {
+                    sortColumn(
+                        this.state.columns,
+                        column,
+                        this.setState.bind(this)
+                    );
+                  }
+
+                }
+  }
+ }
+
+ onSearch(search) {
+   this.setState({
+       search: search
+   });
+ }
+ onSelect(page) {
+    var pagination = this.state.pagination || {};
+
+    pagination.page = page;
+
+    this.setState({
+        pagination: pagination
+    });
+}
+
+onPerPage(e) {
+    var pagination = this.state.pagination || {};
+
+    pagination.perPage = parseInt(event.target.value, 10);
+
+    this.setState({
+        pagination: pagination
+    });
+}
+
+addWords(){
+
+}
+
+
+
+
+
+  render(){
+      var dataPagination = this.props.data;
+      var pagination = this.props.pagination;
+      var header = this.state.header;
+
+      if (this.state.search.query) {
+        // apply search to data
+        // alternatively you could hit backend `onChange`
+        // or push this part elsewhere depending on your needs
+        dataPagination = Search.search(
+            this.state.data,
+            this.state.columns,
+            this.state.search.column,
+            this.state.search.query
+        );
+
+
+      }
+
+      dataPagination = sortColumn.sort(dataPagination, this.state.sortingColumn);
+
+
+      var paginated = Paginator.paginate(dataPagination, pagination);
+
+
+    return (
+      <div>
+      <div className='per-page-container'>
+                        Per page <input type='text' defaultValue={pagination.perPage} onChange={this.onPerPage.bind(this)}></input>
+      </div>
+        <div className='search-container'>
+                Search <Search columns={this.state.columns} data={this.state.data} onChange={this.onSearch.bind(this)}></Search>
+        </div>
+          <Table columns={this.props.columns} data={paginated.data} header={header}></Table>
+          <div className='pagination'>
+              <Paginator
+                  page={paginated.page}
+                  pages={paginated.amount}
+                  beginPages={3}
+                  endPages={3}
+                  onSelect={this.onSelect.bind(this)}>
+             </Paginator>
+          </div>
+      </div>
+
+    )
+  }
+}
+
+export default UITable
