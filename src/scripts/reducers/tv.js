fe840484d1d61e5d2ad51d35c6d88e4d2d6df5b6
gabriel <ggarciaseco@gmail.com>
2017-10-28 06:22:48 +0200

    Fix reducer TV

--- src/scripts/reducers/tv.js
+++ src/scripts/reducers/tv.js
@@ -7,7 +7,10 @@ const initialState = {
 export default function TV (state = initialState, action) {
   switch (action.type) {
     case RECEIVE_TV:
-      return state.TV = action.TV;
+      return {
+        ...state,
+        TV: action.payload
+      };
     default:
       return state;
   }
