e0a2060df7bed890424dda9e19b91c6c5f05434b
gabriel <ggarciaseco@gmail.com>
2017-10-28 10:31:47 +0200

    AddBook refactor

--- src/scripts/components/Interfaces/AddBook.js
+++ src/scripts/components/Interfaces/AddBook.js
@@ -1,37 +1,53 @@
+// @flow
 import React from 'react';
 import { connect } from 'react-redux';
-import {addOneBook} from '../../actions';
+import { addOneBook } from '../../actions';
+import type { DispatchProps, HistoryProps } from './../../types';
 
+type State = {
+  airdate: string,
+  color: string,
+  imagen: string,  
+  nombre: string,
+  overview: string,
+  youtube: string,
+}
 
+type Props = DispatchProps & HistoryProps;
 
-class AddBook extends React.Component {
+class AddBook extends React.Component<void, Props, State> {
+  state: State;
 
   constructor(props) {
     super(props);
-    this.state = {inputName: '' };
+    this.state = {
+      airdate: '',
+      color: '',
+      imagen: '',
+      nombre: '',
+      overview: '',
+      youtube: ''
+    };
   }
 
   handleForm(e){
     e.preventDefault();
 
-    const obj = {
-      nombre: this.refs.name.value,
-      youtube: this.refs.youtube.value,
-      imagen: this.refs.imagen.value,
-      airdate: this.refs.airdate.value,
-      overview: this.refs.overview.value,
-      color: this.refs.color.value
-    };
-
-
     const { dispatch } = this.props;
 
-    dispatch(addOneBook(obj, res => {
+    dispatch(addOneBook(this.state, res => {
       this.props.history.push('/books');
     }));
+  }
 
-
-
+  onChange(evt) {
+    const { name, value } = evt.target;
+    this.setState(prevState => {
+      return {
+        ...prevState,
+        [name]: value
+      };
+    });
   }
 
   render() {
@@ -39,20 +55,16 @@ class AddBook extends React.Component {
     return(
       <form onSubmit={this.handleForm.bind(this)} id="addFilm" method="post" role="form">
         <label className="is-required">Nombre</label>
-        <input ref="name" className={this.state.inputName} type="text" name="name" required placeholder="Nombre"
-               ></input>
+        <input type="text" name="nombre" value={this.state.nombre} required placeholder="Nombre" onChange={(evt) => this.onChange(evt)}></input>
         <label>Youtube</label>
-        <input ref="youtube" className={this.state.inputName} type="text" name="youtube" placeholder="Youtube"
-               ></input>
+        <input type="text" name="youtube" value={this.state.youtube} placeholder="Youtube" onChange={(evt) => this.onChange(evt)}></input>
         <label className="is-required">Imagen</label>
-        <input ref="imagen" className={this.state.inputName} type="text" name="photo" required placeholder="Imagen"
-                ></input>
+        <input ref="imagen" type="text" name="imagen" value={this.state.imagen} required placeholder="Imagen" onChange={(evt) => this.onChange(evt)}></input>
         <label className="is-required">Fecha</label>
-        <input ref="airdate" className={this.state.inputName} type="text" name="airdate" required placeholder="Fecha"
-                        ></input>
+        <input ref="airdate" type="text" name="airdate" value={this.state.airdate} required placeholder="Fecha" onChange={(evt) => this.onChange(evt)}></input>
         <label className="is-required">Descripción</label>
-        <textarea ref="overview" ></textarea>
-        <input ref="color" type="text" name="color" placeholder="Color" autoComplete="off"></input>
+        <textarea name="overview" value={this.state.overview} onChange={(evt) => this.onChange(evt)} ></textarea>
+        <input type="text" name="color" value={this.state.color} placeholder="Color" autoComplete="off" onChange={(evt) => this.onChange(evt)}></input>
         <input type="submit" value="ENVIAR"/>
       </form>
     );
