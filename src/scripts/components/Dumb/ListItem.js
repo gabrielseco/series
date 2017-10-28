0be09aba3c2cbe839a92e9143a938306ba54334b
gabriel <ggarciaseco@gmail.com>
2017-10-28 15:42:19 +0200

    ListItem refactor

--- src/scripts/components/Dumb/ListItem.js
+++ src/scripts/components/Dumb/ListItem.js
@@ -1,37 +1,52 @@
+// @flow
 import React from 'react';
-import {VelocityComponent} from 'velocity-react';
+import { VelocityComponent } from 'velocity-react';
 import styles from 'styles/_films.scss';
-
 require('velocity-animate');
 require('velocity-animate/velocity.ui');
 
-export default class ListItem extends React.Component {
-    constructor(props) {
+type Props = {
+  palabras: string,
+  data: {
+    imagen: string,
+    nombre: string,
+    temporada: ?string,
+  },
+  modify: Function,
+  openModal: Function  
+}
+
+export default class ListItem extends React.Component<void, Props, void> {
+    velocity: any
+    height: number = 345;
+    width: number = 230
+    constructor(props: Props) {
       super(props);
     }
 
     pulse(){
-      this.refs.velocity.runAnimation();
+      this.velocity.runAnimation();
     }
 
     render() {
 
-      let animation = "callout.pulse";
+      const animation = "callout.pulse";
+
+      const temporada = this.props.data.temporada ? this.props.data.temporada : '';
 
       let alt = this.props.data.temporada === undefined
                 ? this.props.data.nombre
-                : this.props.data.nombre + " Season " + this.props.data.temporada;
-
+                : this.props.data.nombre + " Season " + temporada;
 
       return (
-        <VelocityComponent ref="velocity" animation={animation} duration={1000}>
+        <VelocityComponent ref={input => this.velocity = input} animation={animation} duration={1000}>
           <div className={styles.show__image} onMouseOver={this.pulse.bind(this)}>
               <img onClick={this.props.modify.bind(this, this.props.data)}
                    src={this.props.data.imagen}
                    title={alt}
                    alt={alt}
-                   width="230"
-                   height="345"/>
+                   width={this.width}
+                   height={this.height}/>
               <input type="button"
                      className={styles.delete}
                      value="BORRAR"
